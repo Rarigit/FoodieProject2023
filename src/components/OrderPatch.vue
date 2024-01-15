cc31ad452add31d38a8c6c184bf7a7319580962f
AbuTech <Idsrari@outlook.com>
2024-01-14 19:51:23 -0700

    Changed v-text-field from a user True/False to a v-switch toggle switch where the default is not cancelled. In teh data return statement cancelOrder is set to false. I then used a variable isCancelledConverted and setthe toggle button to a boolean 1 or 0. This was because my database is expecting a 1/0 so it had to be converted before the axios request was sent.

--- src/components/OrderPatch.vue
+++ src/components/OrderPatch.vue
@@ -16,11 +16,18 @@
                     label="Enter orderId"
                     prepend-icon="mdi-bio"
                     />
-                    <v-text-field class="mx-auto"
-                    v-model="cancelOrder"
+                    <v-switch
+                        v-model="cancelOrder"
+                        label="cancelOrder"
+                        prepend-icon="mdi-cancel"
+                        :value="cancelOrder"
+                        color="red"
+                    ></v-switch>
+                    <!-- <v-text-field class="mx-auto" -->
+                    <!-- v-model="cancelOrder"
                     label="Enter (True/False)"
-                    prepend-icon="mdi-bio"
-                    />
+                    prepend-icon="mdi-bio" -->
+                    <!-- /> -->
                     <v-spacer></v-spacer>
                     <v-btn color="green" large class="mx-auto styleButton" @click="editClient">Save changes</v-btn>
                 </v-row>
@@ -44,12 +51,15 @@ import cookies from "vue-cookies";
                 // apiKey: process.env.VUE_APP_API_KEY,
                 url: process.env.VUE_APP_API_URL,
                 clientToken: cookies.get('clientToken'),
+                clientId: cookies.get('client'),
                 orderId: cookies.get('orderId'),
-                cancelOrder: "",
+                cancelOrder: false, //Default value as false
             }
         },
         methods: {
             editClient() {
+                // Convert 'True'/'False' string to 1/0
+                let isCancelledConverted = this.cancelOrder ? 1 : 0;
                 axios.request({
                     method : "PATCH",
                     url: this.url + "/order-client",
@@ -59,7 +69,9 @@ import cookies from "vue-cookies";
                     },
                     data : {
                         orderId: this.orderId,
-                        cancelOrder: this.cancelOrder,//Mark set this to true
+                        clientId: this.clientId,
+                        cancelOrder: isCancelledConverted
+                        // cancelOrder: this.cancelOrder,//Mark set this to true
                     }
                     }).then((response)=>{
                     console.log(response);
